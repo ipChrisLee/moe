71fc749eb66187a3e9907fec005f50dc6bd9f7a0
ChrisLee <chirsleecn@outlook.com>
2022-09-26 15:00:09 +0800

    Feat : Add time support.

---
+++ moe/time_pro.cpp
@@ -0,0 +1,61 @@
+#include "time_pro.hpp"
+
+#include <sstream>
+
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+
+
+namespace moe::now {
+
+int day() {
+	std::time_t
+		t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::tm ltime{ };
+	localtime_r(&t, &ltime);
+	std::stringstream ss;
+	ss << std::put_time(&ltime, "%d") << std::endl;
+	auto res = i32(0);
+	ss >> res;
+	return res;
+}
+
+int hour() {
+	std::time_t
+		t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::tm ltime{ };
+	localtime_r(&t, &ltime);
+	std::stringstream ss;
+	ss << std::put_time(&ltime, "%H") << std::endl;
+	auto res = i32(0);
+	ss >> res;
+	return res;
+}
+
+int minute() {
+	std::time_t
+		t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::tm ltime{ };
+	localtime_r(&t, &ltime);
+	std::stringstream ss;
+	ss << std::put_time(&ltime, "%M") << std::endl;
+	auto res = i32(0);
+	ss >> res;
+	return res;
+}
+
+int second() {
+	std::time_t
+		t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::tm ltime{ };
+	localtime_r(&t, &ltime);
+	std::stringstream ss;
+	ss << std::put_time(&ltime, "%S") << std::endl;
+	auto res = i32(0);
+	ss >> res;
+	return res;
+}
+}
+
---
+++ moe/time_pro.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <moe_typedef.hpp>
+
+
+namespace moe::now {
+
+//  Day of month.
+i32 day();
+
+//  Hour of day, 24 split.
+i32 hour();
+
+//  Minute of hour, 60 split.
+i32 minute();
+
+//  Second of minute, 60 split.
+i32 second();
+
+}
+
+
+
