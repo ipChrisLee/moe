917a50fda808f412eecd771c9f07bddae7f4ff5e
ChrisLee <chirsleecn@outlook.com>
2022-11-24 18:03:18 +0800

    Feat : Add some other integer typede.

--- moe/moe_typedef.hpp
+++ moe/moe_typedef.hpp
@@ -1,10 +1,18 @@
+/* WARNING:
+ *      Include this file may cause double definition problem(such as, you have defined u8 before.).
+ *
+ */
 #pragma once
 
 #include <cstdint>
 
 
-using i8 = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
 using i64 = int64_t;
+using i32 = int32_t;
+using i16 = int16_t;
+using i8 = int8_t;
+
+using u64 = uint64_t;
 using u32 = uint32_t;
+using u16 = uint16_t;
+using u8 = uint8_t;
