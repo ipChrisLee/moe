9f6101e8d5067ed45bc4464b70c86b76af127b65
ChrisLee <chirsleecn@outlook.com>
2023-03-26 15:28:54 +0800

    Feat : Now moe_assert can print not only const char *

--- moe/rt_check.hpp
+++ moe/rt_check.hpp
@@ -36,7 +36,7 @@ std::string_view get_exit_info(ExitType panicType);
 
 #define moe_assert_with_msg(b, msg) if(1){ \
     if(!(b)){                   \
-        std::cerr << "Assert [" #b "] failed at [" MOE_CODE_POS "] with message [" msg "]." << std::endl; \
+        std::cerr << "Assert [" #b "] failed at [" MOE_CODE_POS "] with message [" << msg << "]." << std::endl; \
         moe::moe_exit(moe::ExitType::assert_failed);                             \
     }                         \
 }
