a60e317196ae26b16e2772d8cc24bbcf628b4edc
ChrisLee <chirsleecn@outlook.com>
2023-04-01 12:35:56 +0800

    Feat : Rename overload to moe::type_match

--- moe/overload.hpp
+++ moe/overload.hpp
@@ -1,10 +1,12 @@
 //	Ref: https://stackoverflow.com/a/64018031
 #pragma once
 
+namespace moe {
 template<typename...Ts>
-struct overload : Ts ... {
+struct type_match : Ts ... {
 	using Ts::operator()...;
 };
 
 template<typename...Ts>
-overload(Ts...)->overload<Ts...>;
+type_match(Ts...)->type_match<Ts...>;
+}
